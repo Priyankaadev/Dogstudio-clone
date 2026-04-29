5b31915f20e4324be60f1032d250da3d8e09d90e
Priyankaadev <priyaaswal79@gmail.com>
2026-04-29 22:12:43 +0530

    added animations-dogmodel

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,7 +1,8 @@
 import {  useThree } from "@react-three/fiber";
-import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
+import { OrbitControls, useAnimations, useGLTF, useTexture } from "@react-three/drei";
 import { color, normalMap, texture } from "three/tsl";
 import * as THREE from "three";
+import { useEffect } from "react";
 
 
 const Dog = ()=>{
@@ -15,6 +16,12 @@ const Dog = ()=>{
 
     })
 
+    const {actions} = useAnimations(model.animations, model.scene)
+
+    useEffect(()=>{
+        actions["Take 001"].play()
+
+    },[actions])
     const [
         normalMap,
         sampleMatCap
